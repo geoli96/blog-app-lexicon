b121d2995c6673d5d2ef500325a3932982c6dca5
Georgij Li <ligeorgij7@hotmail.com>
2026-09-05 14:40:28 +0200

    use atomic copy

--- proxy.ts
+++ proxy.ts
@@ -1,15 +1,22 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 import fs from 'fs/promises'
-
-//export default NextAuth(authConfig).auth;
+const writeFileAtomic = require('write-file-atomic')
 
 export default async function proxy(request:any) {
   if(request.method === "POST" || request.method === "PUT"){
-     await fs.copyFile("db.json", "dbCopy.json");
-    console.log("Copied db");
+    try {
+      const db = await fs.readFile("db.json");
+      await writeFileAtomic('dbCopy.json', db)
+      console.log("Copied db");
+
+      return NextAuth(authConfig).auth(request);
+    } catch (error) {
+      console.log(error)
+    }
+  }else{
+    return NextAuth(authConfig).auth(request);
   }
-  return NextAuth(authConfig).auth(request)
 }
 
 export const config = {
